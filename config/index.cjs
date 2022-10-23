51a9975c517c8fe6ceee29ca8d2a3a6a2a08dc4f
Xie <33503980+xjc5772@users.noreply.github.com>
2022-10-24 02:13:36 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oqUSw6mndXaDSbI01bCWRz4Xn5CA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'Sm5GOjjabg2htlvOcfDhLJu3egZtbyavvnTWtF3wM24	',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-19',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'pICIJ12syGWykTQIxv0NSn9zMq80JKJSaF3RVQps1rM	',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oqUSw6rADtBCtca8-OyVcF7ICiPA	',
     }
   ],
 
